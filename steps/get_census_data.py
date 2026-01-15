88ef1b8a825b3e93f0103c7e584e4b9906c37e42
jkolberg <jwkolberg@gmail.com>
2026-01-15 10:34:36 -0800

    move census year to settings.yaml

--- steps/get_census_data.py
+++ steps/get_census_data.py
@@ -6,13 +6,14 @@ def get_dec_block_data(pipeline):
     p = pipeline
     api_key = os.getenv(p.settings['CensusKey'])
     c = CensusApi(api_key)
+    census_year = p.settings.get('census_year')
 
     county_ids = p.settings['county_ids']
     state_id = p.settings['state_id']
     dec_cols_dict = p.settings['census_variables']
 
     dec = (
-    c.get_dec_data(dec_cols_dict, 2020, 'block', 'pl', county_ids,state_id)
+    c.get_dec_data(dec_cols_dict, census_year, 'block', 'pl', county_ids,state_id)
     .drop(columns='name')
     )
 
